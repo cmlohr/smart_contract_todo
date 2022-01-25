747f10909ffad328c2c272bfe3508fe3f2bb9d67
cmlohr <cmlohr@outlook.com>
2022-01-25 17:36:05 -0600

    created mapping for tasks

--- contracts/Todo.sol
+++ contracts/Todo.sol
@@ -3,4 +3,21 @@ pragma solidity ^0.5.0;
 contract Todo {
     uint public taskCount = 0;
 
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+       addTask("It works!");
+    }
+
+    function addTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
 }
