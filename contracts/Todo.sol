37b0201972a45ae5e148898daf1c87f90c508331
cmlohr <cmlohr@outlook.com>
2022-01-25 19:18:16 -0600

    fin

--- contracts/Todo.sol
+++ contracts/Todo.sol
@@ -1,23 +1,42 @@
 pragma solidity ^0.5.0;
 
 contract Todo {
-    uint public taskCount = 0;
+  uint public taskCount = 0;
 
-    struct Task {
-        uint id;
-        string content;
-        bool completed;
-    }
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
 
-    mapping(uint => Task) public tasks;
+  mapping(uint => Task) public tasks;
 
-    constructor() public {
-       addTask("It works!");
-    }
+  event TaskCreated(
+    uint id,
+    string content,
+    bool completed
+  );
 
-    function addTask(string memory _content) public {
-        taskCount ++;
-        tasks[taskCount] = Task(taskCount, _content, false);
-    }
+  event TaskCompleted(
+    uint id,
+    bool completed
+  );
+
+  constructor() public {
+    createTask("It works!");
+  }
+
+  function createTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+    emit TaskCreated(taskCount, _content, false);
+  }
+
+  function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+    emit TaskCompleted(_id, _task.completed);
+  }
 
 }
